6ca3bc56302de969260e4721ff7c19ccb115f2b7
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-04-13 08:03:44 -0400

    added repl, cd, ls, base.run() try/catch

---
+++ src/UnixRepl.jl
@@ -0,0 +1,28 @@
+module UnixRepl
+using ReplMaker
+function parse_command(s)
+    dims = split(s, " ")
+    command = dims[1]
+    if length(dims) != 1
+        param = dims[2]
+    end
+    if command == "cd"
+        cd(param)
+    elseif command == "ls"
+        readdir()
+    elseif command == "pwd"
+        pwd()
+    else
+        try
+            run(Cmd(s))
+        catch
+            println("Could not determine command")
+        end
+    end
+end
+initrepl(parse_command,
+    prompt_text="bash> ",
+    prompt_color = :yellow,
+    start_key='[',
+    mode_name="bash")
+end
